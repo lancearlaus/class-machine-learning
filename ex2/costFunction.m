394e4069ad1e8f69cf956cbaace5b4076881f019
Lance Arlaus <lance.arlaus@gmail.com>
2015-11-10 16:51:46 -0500

    Exercise 2 in progress

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -21,9 +21,9 @@ grad = zeros(size(theta));
 %
 
 
+J = 1/m * sum((-y .* log(sigmoid(X * theta))) - ((1 - y) .* log(1 - sigmoid(X * theta))), 1);
 
-
-
+grad = (X' * (sigmoid(X * theta) - y)) ./ m;
 
 
 
