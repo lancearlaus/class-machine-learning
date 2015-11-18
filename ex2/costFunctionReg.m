4cd35a8484aaba27316cf408c00fda2d69aaf1f2
Lance Arlaus <lance.arlaus@gmail.com>
2015-11-18 11:56:19 -0500

    Submitted exercise 2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,9 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% theta values for regularization (all but first)
+rtheta = [0; theta(2:end)]
 
+J = 1/m * (sum((-y .* log(sigmoid(X * theta))) - ((1 - y) .* log(1 - sigmoid(X * theta))), 1) + ((lambda / 2) * (rtheta' * rtheta)));
 
-
+grad = ((X' * (sigmoid(X * theta) - y)) + (lambda * rtheta)) ./ m;
 
 
 % =============================================================
